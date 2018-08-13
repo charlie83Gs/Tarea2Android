938023c58c33d139bafc5a65234fd381c0d2d716
charlie <carlosgomezsoza@gmail.com>
2018-08-13 09:34:17 -0600

    Login y algunas clases

--- app/src/main/java/cr/ac/tec/tarea2/LoginActivity.java
+++ app/src/main/java/cr/ac/tec/tarea2/LoginActivity.java
@@ -2,14 +2,25 @@ package cr.ac.tec.tarea2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText username;
+    private EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+
+        username = (EditText)findViewById(R.id.edt_LoginName);
+        password = (EditText)findViewById(R.id.edt_LoginPassword);
+    }
+
+    public void login(View view){
+        String name = username.getText().toString();
+        String pass = password.getText().toString();
     }
 }
