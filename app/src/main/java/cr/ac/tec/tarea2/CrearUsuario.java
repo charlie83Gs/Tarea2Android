6a81ab95c20bf27f6d6168b23953d1781349f034
charlie83Gs <carlosgomezsoza@gmail.com>
2018-08-16 15:50:39 -0600

    crearUsuario

--- app/src/main/java/cr/ac/tec/tarea2/CrearUsuario.java
+++ app/src/main/java/cr/ac/tec/tarea2/CrearUsuario.java
@@ -2,12 +2,22 @@ package cr.ac.tec.tarea2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class CrearUsuario extends AppCompatActivity {
+
+    EditText nombre;
+    EditText alias;
+    EditText correo;
+    EditText nombre;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_usuario);
+    }
+
+    public void crearUsuario(View view){
+
     }
 }
